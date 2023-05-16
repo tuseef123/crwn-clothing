7639f53460e363f3afe04758d01eac62061deaa4
tuseef123 <tossefashraf.292791@gmail.com>
2023-05-16 18:40:56 +0500

    added shop page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import "@fontsource/open-sans-condensed";
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/homepage/shop/shop.component';
 import {Route,Switch } from 'react-router-dom';
 
 const HatsPage = () =>{
@@ -10,12 +11,21 @@ const HatsPage = () =>{
     </div>
   )
 }
+const WomenPage = () =>{
+  return(
+    <div>
+      <h1>WomenPage</h1>
+    </div>
+  )
+}
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop' component={ShopPage} />
       <Route exact path='/hats' component={HatsPage} />
+      <Route exact path='/shop/womens' component={WomenPage} />
       </Switch>
     </div>
   );

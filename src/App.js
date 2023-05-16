31d6b7d2ab40267278bd3e68dfbbbfd57798ec6f
tuseef123 <tossefashraf.292791@gmail.com>
2023-05-16 12:30:50 +0500

    router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import "@fontsource/open-sans-condensed";
 import HomePage from './pages/homepage/homepage.component';
+import {Route,Switch } from 'react-router-dom';
+
+const HatsPage = () =>{
+  return(
+    <div>
+      <h1>HatsPage</h1>
+    </div>
+  )
+}
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,7 +12,7 @@ export default class Directory extends Component {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
@@ -47,8 +47,8 @@ export default class Directory extends Component {
     return (
         <div className="directory-menu">
             {
-                this.state.sections.map(({title,imageUrl,id,size})=>{
-                    return <MenuItem key={id} imageUrl={imageUrl} size={size} title={title} />
+                this.state.sections.map(({id,...otherSectionProps})=>{
+                    return <MenuItem key={id} {...otherSectionProps} />
                 })
             }
         </div>
